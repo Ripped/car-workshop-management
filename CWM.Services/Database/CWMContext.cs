6496a59d903aaba2fb1a8cb76702442a2c6bf928
Ripped <belmin.ibric98@gmail.com>
2023-12-17 13:37:22 +0100

    feat: project-init

---
+++ CWM.Services/Database/CWMContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWM.Services.Database
+{
+    public class CWMContext : DbContext
+    {
+        public CWMContext(DbContextOptions<CWMContext> options) : base(options) { }
+    }
+}
